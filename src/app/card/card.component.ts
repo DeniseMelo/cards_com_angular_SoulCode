b29527b9d33da3c12c17db5d8811804be38aa88a
DeniseMelo <melodenise.ti@gmail.com>
2022-03-11 13:18:34 -0300

    primeiro commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() titulo = '';
+  @Input() preco = '';
+  @Input() conteudo: string[] = [];
+  @Input() cor = '#ffffff';
+  @Input() corTexto = '#000000';
+
+  constructor(private snackBar: MatSnackBar) { }
+    
+  ngOnInit(): void {
+  }
+
+  onClickLearnMore(): void {
+    this.snackBar.open("Pacote comprado! Mas, tenha cuidado ao gastar seu dinheiro com bobagens na internet", "OK");
+  }
+
+}
+
